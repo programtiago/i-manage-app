aba936903fbf81c29d94d9e260c4d6aea490f5e5
Tiago Silva <programtiago@gmail.com>
2025-07-14 12:01:16 +0100

    Initialization of project

---
+++ backend/src/test/java/com/devtiago/i_manage_app/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtiago.i_manage_app.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

d953ffcd9f3c2961acfee92c0f76b8bb3acc1e2c
Tiago Silva <programtiago@gmail.com>
2025-07-14 12:41:57 +0100

    Add findAll and create endpoints for Employee

--- backend/src/main/java/com/devtiago/i_manage_app/backend/entity/controller/EmployeeController.java
+++ backend/src/main/java/com/devtiago/i_manage_app/backend/entity/controller/EmployeeController.java
@@ -1,12 +1,29 @@
 package com.devtiago.i_manage_app.backend.entity.controller;
 
+import com.devtiago.i_manage_app.backend.entity.Employee;
+import com.devtiago.i_manage_app.backend.entity.dto.EmployeeDto;
 import com.devtiago.i_manage_app.backend.entity.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/employees")
 @RequiredArgsConstructor
 public class EmployeeController {
 
     private final EmployeeService employeeService;
+
+    @GetMapping
+    public List<EmployeeDto> findAll(){
+        return employeeService.getAllEmployees();
+    }
+    @PostMapping
+    public EmployeeDto create(Employee employee){
+        return employeeService.create(employee);
+    }
 }
--- backend/src/main/java/com/devtiago/i_manage_app/backend/entity/service/EmployeeService.java
+++ backend/src/main/java/com/devtiago/i_manage_app/backend/entity/service/EmployeeService.java
@@ -1,12 +1,28 @@
 package com.devtiago.i_manage_app.backend.entity.service;
 
+import com.devtiago.i_manage_app.backend.entity.Employee;
+import com.devtiago.i_manage_app.backend.entity.dto.EmployeeDto;
 import com.devtiago.i_manage_app.backend.repository.EmployeeRepository;
+import com.devtiago.i_manage_app.backend.utils.mapper.EmployeeMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
+    private final EmployeeMapper employeeMapper;
+
+    public List<EmployeeDto> getAllEmployees() {
+        return employeeRepository.findAll().stream()
+                .map(employeeMapper::toDto)
+                .collect(Collectors.toList());
+    }
+    public EmployeeDto create(Employee employee){
+        return employeeMapper.toDto(employeeRepository.save(employee));
+    }
 }
